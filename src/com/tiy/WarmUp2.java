d563e3245a152b95dedcc9ad8f5c7431857ba445
Josh O'Brian <joshuaeobrian@gmail.com>
2017-03-23 22:53:57 -0400

    First Commit

---
+++ src/com/tiy/WarmUp2.java
@@ -0,0 +1,33 @@
+package com.tiy;
+
+/**
+ * Created by josh on 3/22/17.
+ */
+public class WarmUp2 {
+
+
+
+	public String stringYak(String str) {
+		return str.replaceAll("yak","");
+	}
+
+
+	public String stringTimes(String str, int n) {
+		String newString="";
+
+		for(int i = 1; i <= n; i++){
+			newString+=str;
+
+		}
+		return newString;
+	}
+
+	public String stringBits(String str) {
+		String nString = "";
+		for(int i = 0; i < str.length();i+=2){
+			nString += str.charAt(i);
+		}
+		return nString;
+	}
+
+}
---
+++ test/com.tiy/WarnUp2Test.java
@@ -0,0 +1,162 @@
+package com.tiy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+/**
+ * Created by josh on 3/22/17.
+ */
+public class WarnUp2Test {
+	private WarmUp2 warmUp2;
+	@Before
+	public void before(){
+		warmUp2 = new WarmUp2();
+	}
+
+
+//Remove the Yak
+	@Test
+	public void testForYak1() throws Exception{
+		String x = warmUp2.stringYak("Bakyakattack");
+		assertThat(x, equalTo("Bakattack"));
+	}
+	@Test
+	public void testForYak2() throws Exception{
+		String x = warmUp2.stringYak("workarkya");
+		assertThat(x, equalTo("workarkya"));
+	}
+	@Test
+	public void testForYak3() throws Exception{
+		String x = warmUp2.stringYak("yaksack");
+		assertThat(x, equalTo("sack"));
+	}
+	@Test
+	public void testForYak4() throws Exception{
+		String x = warmUp2.stringYak("lacktackyak");
+		assertThat(x, equalTo("lacktack"));
+	}
+	@Test
+	public void testFor5() throws Exception{
+		String x = warmUp2.stringYak("yakpak");
+		assertThat(x, equalTo("pak"));
+	}
+	@Test
+	public void testForYak6() throws Exception{
+		String x = warmUp2.stringYak("xxxyakyyyakzzz");
+		assertThat(x, equalTo("xxxyyzzz"));
+	}
+
+	@Test
+	public void testForYak7() throws Exception{
+		String x = warmUp2.stringYak("HiyakHi");
+		assertThat(x, equalTo("HiHi"));
+	}
+	@Test
+	public void testForYak8() throws Exception{
+		String x = warmUp2.stringYak("");
+		assertThat(x, equalTo(""));
+	}
+	@Test
+	public void testForYak9() throws Exception{
+		String x = warmUp2.stringYak("baklack");
+		assertThat(x, equalTo("baklack"));
+	}@Test
+	public void testForYak10() throws Exception{
+		String x = warmUp2.stringYak("yakxxxyak");
+		assertThat(x, equalTo("xxx"));
+	}
+
+
+	//StringTimes
+	@Test
+	public void testStringTimes1() throws Exception{
+		String x = warmUp2.stringTimes("Hi",1);
+		assertThat(x, equalTo("Hi"));
+	}
+	@Test
+	public void testStringTimes2() throws Exception{
+		String x = warmUp2.stringTimes("Hi",2);
+		assertThat(x, equalTo("HiHi"));
+	}
+	@Test
+	public void testStringTimes3() throws Exception{
+		String x = warmUp2.stringTimes("Hi",3);
+		assertThat(x, equalTo("HiHiHi"));
+	}
+	@Test
+	public void testStringTimes4() throws Exception{
+		String x = warmUp2.stringTimes("x",4);
+		assertThat(x, equalTo("xxxx"));
+	}
+
+	@Test
+	public void testStringTimes5() throws Exception{
+		String x = warmUp2.stringTimes("Hi",5);
+		assertThat(x, equalTo("HiHiHiHiHi"));
+	}
+
+	@Test
+	public void testStringTimes6() throws Exception{
+		String x = warmUp2.stringTimes("code",3);
+		assertThat(x, equalTo("codecodecode"));
+	}
+
+	@Test
+	public void testStringTimes7() throws Exception{
+		String x = warmUp2.stringTimes("code",2);
+		assertThat(x, equalTo("codecode"));
+	}
+
+	@Test
+	public void testStringBits1() throws Exception{
+		String x = warmUp2.stringBits("Hello");
+		assertThat(x, equalTo("Hlo"));
+	}
+	@Test
+	public void testStringBits2() throws Exception{
+		String x = warmUp2.stringBits("Hi");
+		assertThat(x, equalTo("H"));
+	}
+	@Test
+	public void testStringBits3() throws Exception{
+		String x = warmUp2.stringBits("Heeololeo");
+		assertThat(x, equalTo("Hello"));
+	}
+	@Test
+	public void testStringBits4() throws Exception{
+		String x = warmUp2.stringBits("");
+		assertThat(x, equalTo(""));
+	}
+	@Test
+	public void testStringBits5() throws Exception{
+		String x = warmUp2.stringBits("Greetings");
+		assertThat(x, equalTo("Getns"));
+	}
+	@Test
+	public void testStringBits6() throws Exception{
+		String x = warmUp2.stringBits("Chocoate");
+		assertThat(x, equalTo("Coot"));
+	}
+	@Test
+	public void testStringBits7() throws Exception{
+		String x = warmUp2.stringBits("pi");
+		assertThat(x, equalTo("p"));
+	}
+	@Test
+	public void testStringBits8() throws Exception{
+		String x = warmUp2.stringBits("Hello Kitten");
+		assertThat(x, equalTo("HloKte"));
+	}
+	@Test
+	public void testStringBits9() throws Exception{
+		String x = warmUp2.stringBits("hxaxpxpxy");
+		assertThat(x, equalTo("happy"));
+	}
+
+
+}
